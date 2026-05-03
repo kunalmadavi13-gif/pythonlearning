dff35db8cfa54ba2d0d7d3adeff8fd29c5b40334
kunalmadavi13 <kunalmadavi13@gmail.com>
2026-05-03 13:42:36 +0530

    added while

---
+++ LocalRepo/star_program_with_while_loop.py
@@ -0,0 +1,20 @@
+'''
+* 
+* * 
+* * * 
+* * * *
+'''
+
+def first_piramid():
+    i = 1
+    while(i < 6):
+        print("*"*i)
+        i = i + 1
+
+def second_piramid():
+    i = 5
+    while(i > 0):
+        print("*"*i)
+        i = i - 1
+
+second_piramid()
